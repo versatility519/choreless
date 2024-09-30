a0b15c5b82f99cb0b99b1426a3e5407dd397def8
stefan <veteransmut519@gmail.com>
2024-09-30 13:48:01 +1000

    feat: font-familly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 
 // Remove or comment out the ErrorBoundary import for now
 // import ErrorBoundary from '../components/ErrorBoundary';
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Choreless",
@@ -29,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-slate-100 antialiased`}
+        className={`bg-slate-100 antialiased`}
       >
         {/* Remove ErrorBoundary component for now */}
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="bg-slate-100">
       <Navbar />
-      <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
 
           {/* <section className="bg-white py-12 px-4 sm:px-6 lg:px-8"> */}
